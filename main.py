0a0c11f4e487f98af35bd7c3b4da5f7c927d246b
MigueEC <filia.castillo@gmail.com>
2025-05-30 09:22:07 -0500

    Create Env

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Main script for the application
+"""
+
+import random 
+
+def main():
+    """
+    Main function for the application
+    """
+    for i in range(10):
+        print(random.randint(1, 10))
+
+if __name__ == "__main__":
+    main()
