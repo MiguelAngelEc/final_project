6019a168b332e51d2cd76d447e747529d610a164
MigueEC <filia.castillo@gmail.com>
2025-05-30 09:35:37 -0500

    Create Board

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ def main():
     """
     Main function for the application
     """
-    for i in range(10):
-        print(random.randint(1, 10))
+    board = create_board()
+    print_board(board)
+        
+def create_board(size=4):
+    board = [['*' for _ in range (size)] for _ in range (size)]
+    return board
+
+def print_board(board):
+    size = len(board)
+    columns = "    " + "   ".join(str(i + 1) for i in range(size))
+    print(columns)
+    for i in range(size):
+        row = chr(65 + i) + "   " + "   ".join(board[i])
+        print(row)
 
 if __name__ == "__main__":
     main()
